fc79baf901a618d39b246f3c77e307f00aa52562
Piyush Singh <piyush.singh3>
2021-02-25 08:07:26 +0530

    login validation :construction:

--- src/module/login/login.js
+++ src/module/login/login.js
@@ -4,39 +4,44 @@ import { doLogin } from "services/login/login";
 import "./login.scss";
 
 const Login = () => {
-  let history = useHistory(); 
+  let history = useHistory();
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
 
   const onLogin = () => {
-    doLogin(userName, password).then((response) => {
-      console.log(response);
-      history.push("/movies");
-    });
+    doLogin(userName, password)
+      .then((response) => {
+        history.push("/movies");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
 
   return (
     <div className="login_container">
-      <div className="credentails">
-        User Name:
-        <input
-          placeholder="User Name"
-          type="text"
-          value={userName}
-          onChange={(e) => setUserName(e.target.value)}
-        />
-      </div>
-      <div className="credentails">
-        Password:
-        <input
-          placeholder="Password"
-          type="text"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <div className="credentails">
-        <button onClick={onLogin}>Sign In</button>
+      <div className="enclosed_container">
+        <div className="credentails">
+          <span>User Name:</span>
+          <input
+            placeholder="User Name"
+            type="text"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </div>
+        <div className="credentails">
+          <span>Password:</span>
+          <input
+            placeholder="Password"
+            type="text"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="credentails">
+          <button onClick={onLogin}>Sign In</button>
+        </div>
       </div>
     </div>
   );
--- src/services/login/login.js
+++ src/services/login/login.js
@@ -1,9 +1,14 @@
 import { Auth } from "module/authentication/auth";
 
-export const doLogin = (userName, Password) =>
-  new Promise((resolve) => {
+export const doLogin = (userName, password) =>
+  new Promise((resolve, reject) => {
     setTimeout(() => {
-      Auth.login("Wookie2021");
-      resolve(`${userName} logged in successfully`);
+      if (userName === 'admin' && password === '1234') {
+        Auth.login("Wookie2021");
+        resolve(`${userName} logged in successfully`);
+      } else {
+        reject('Incorrect user name or password');
+      }
+      
     }, 300);
   });
